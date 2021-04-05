3cd5f1d87eb79fc51a53c5db1f8c4fd4b37fb7bf
Rdnc12 <rdnc12@gmail.com>
2021-04-05 13:43:19 +0200

    scrollview

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  FlatList
+} from "react-native";
 
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState("");
@@ -24,13 +32,13 @@ export default function App() {
         />
         <Button title="Add" onPress={addGoalHandler} />
       </View>
-      <View>
+      <ScrollView>
         {courseGoals.map((goal, index) => (
           <View style={styles.listItem}>
             <Text key={index}>{goal}</Text>
           </View>
         ))}
-      </View>
+      </ScrollView>
     </View>
   );
 }
@@ -51,16 +59,26 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     padding: 10,
   },
-  listItem:{
-    padding:10,
-    marginVertical:10,
-    backgroundColor:'#ccc',
-    borderColor:'black',
-    borderWidth:1
-  }
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+  },
 });
 
 // We can not see textinput component in display,we need to add some styling.
 
 // FLEXBOX: A concept/ set of styling properties that allows you to structure content
 // (create layout...)
+
+/*FlatList is built to render a large list of items. You need to pass an array of data and then render each item in the array with the renderItem callback. It is optimized to have very good performances with very large arrays because it actually only renders the items that need to be displayed at the moment.
+
+ScrollView is built to render a generic content in a way that it scrolls when the content is bigger than the ScrollView itself. You don't pass an array of data, but you put elements inside the ScrollView in the same way you would use a normal View. Be careful however, it does not provide the same optimization of the flat list for very long content.
+
+As a rule of thumb:
+
+Do you need to render a list of similar items from an array? Use FlatList
+
+Do you need to render generic content in a scrollable container? Use ScrollView*/
