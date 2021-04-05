9d3451b9b25946f9ddd145dfee6477881a1bbc62
Rdnc12 <rdnc12@gmail.com>
2021-04-05 12:58:42 +0200

    adding state and event

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  // when we write just goalInputHandler,
+  //onChangeText works every keystroke.
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+  const addGoalHandler = () => {
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+  };
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput
           placeholder="Course Goal"
           style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
         />
-        <Button title="Add" />
+        <Button title="Add" onPress={addGoalHandler} />
       </View>
-      <View/>
+      <View></View>
     </View>
   );
 }
@@ -26,12 +39,12 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     alignItems: "center",
   },
-  input:{
+  input: {
     width: "80%",
     borderBottomColor: "black",
     borderBottomWidth: 1,
     padding: 10,
-  }
+  },
 });
 
 // We can not see textinput component in display,we need to add some styling.
